6448e2db7c62e730abccf81f21f85bb38589ca20
Marcela1902 <casasolamarcela@gmail.com>
2020-05-01 20:21:13 -0500

    act

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const postsRouter = require('./routes/posts')
-const userRouter = require('./routes/USER')
+const USERRouter = require('./routes/USER')
 const authRouter = require('./routes/auth')
 const app = express ()
 
 app.use((request,response, next) =>{
-    console.log(`>[${request.method} ${request.url}] $body: ${JSONS.Stringify(request.body)} `)
+    console.log(`>[${request.method} ${request.url}] $body: ${JSON.Stringify(request.body)} `)
     next()
 })
 
 app.use(express.json())
 app.use('/posts', postsRouter)
-app.use('/USER',userRouter)
+app.use('/USER',USERRouter)
 app.use('/auth',authRouter)
 
  
